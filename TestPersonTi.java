1ba027fda8c3a34a9651b5e800d14471bd158332
我穿西服贼帅 <1020422076@qq>
2021-06-21 08:28:30 +0800

    手动

--- TestPersonTi.java
+++ TestPersonTi.java
@@ -2,7 +2,7 @@ public class TestPersonTi {
     public static void main(String[] args) {
         PersonTi pe=new PersonTi();
         pe.name="黄辉";
-        pe.gender="女";
+        pe.gender="男";
         pe.age=20;
 
     }
